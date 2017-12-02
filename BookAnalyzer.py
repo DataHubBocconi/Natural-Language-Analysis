5637915f37904a8959b45502108b21e1b573838a
Guglielmo Gattiglio <g.gattiglio@gmail.com>
2017-12-02 21:41:19 +0100

    Changed clean_book(), improved cleaning and put debugging. Already tested. Improved create_corpus(). Not yet tested.

--- BookAnalyzer.py
+++ BookAnalyzer.py
@@ -12,18 +12,25 @@ import pandas as pd
 # This function only wants as input the folder where all your txt files are stored.
 # It then joins them all together into a unique txt file called "the great book.txt"
 def create_corpus(directory_name):
+    '''Puts all the .txt. files in a given folder all together.
+    
+    Input: directory name
+    Output: None. Creates a .txt file called 'the_great_book.txt' in the same
+    directory specified in input, containing all the content 
+    of the other files separated by a spaces
+    '''
+    path = os.getcwd()
     os.chdir(directory_name)
     list_of_files = glob.glob('*.txt')
-    unique_text = ''
+    unique_text = []
     for title in list_of_files:
         print(title)
-        book = open(title, 'r', encoding = 'utf-8', errors='ignore')
-        text = book.read()
-        unique_text += text
-        unique_text += ' '
-    the_great_book = open('the_great_book.txt', 'w', encoding ='utf-8', errors='ignore')
-    the_great_book.write(unique_text)
-    the_great_book.close()
+        with open(title, encoding = 'utf-8') as reader:
+            text = reader.read()
+        unique_text.append(text)
+    with open('the_great_book.txt', 'w', encoding ='utf-8') as writer:
+        writer.write(" ".join(unique_text))
+    os.chdir(path)
     return
 
 # This function is the one you need to run to clean and analyze the frequency distribution
@@ -52,37 +59,56 @@ def main(directory_name, book = 'the_great_book'):
     return
 
 
-def clean_book(book):
+def clean_book(book, min_length=4):
+    '''Removes stopwords and unuseful characters from a string.
+    
+    Takes in input the text as string and an optional parameter
+    min_length, the length of all the words in outpt will be at 
+    least min_length. The default is 4.
+    
+    Outputs a list of words.
+    '''
+    os.chdir("d:\\guglielmo documenti\\progetti   programmazione+\\Python\\Leo_immigraz\\Natural-Language-Analysis\\source")
     with open(str(book)+'.txt',encoding='utf-8') as reader:
-	    text = reader.read()
-    # Remove all the characters which are used by Python to go to a new line
-    text = re.sub(r'- \n','', text)
-    text = re.sub(r'\n',' ',text)
-    print('New lines removed')
-    # Here we start cleaning the text, we will tokenize it at the end to make the process 
-    # more efficient
-    # Remove the genitivo sassone
+        text = reader.read()
+    #N.B. the order of the following operations is important!
+    #remove header
+    #text = re.sub(r'.{0,133}', '', text, 1, re.S)
+    #remove links (must be done before symbol rem.)
+    text = re.sub(r'http\S+', '', text)
+    #remove genitivo sassone (must be done before symbol rem.)
     text = re.sub(r'\'s','',text)
-    # Remove everything which is not a letter, thus punctuation and numbers. 
-    # Then it lower-cases everything
-    text = re.sub(r'[^a-zA-z ]', '',  text).lower()
+    #handle new-line that separates whole words (like considered
+    #might become con- \nsidered 
+    text = re.sub(r'- \n','', text) 
+    #remove symbols
+    text = re.sub(r"[^a-zA-Z0-9_\s'-]", '', text)
+    #remove stand-alone words (made of any character
+    #of length from one to min_length: {1,min_length}
+    text = re.sub(r'(?<=\s).{1,'+str(min_length-1)+r'}(?=\s)', '', text)
+    #remove new lines
+    text = re.sub(r'\n',' ',text)
+    text = text.lower()
+    # Removes all the stopwords (words which are not characterizing for the meaning of the text)
+    for word in stop_words.get_stop_words('english'):
+        if len(word) >= min_length:
+            text = re.sub(' '+word+' ',' ',text)
     # Removes all the unnecessary spaces.
     text = re.sub(r' {2,}',' ',text)
-    text = re.sub(r' [a-zA-z] ', ' ', text)
-    print('Non-letters removed')
-    #f = re.sub(r'( a | about | above | after | again | against | all | am | an | and | any | are | aren\'t | as | at | be | because | been | before | being | below | between | both | but | by | can\'t | cannot | could | couldn\'t | did | didn\'t | do | does | doesn\'t | doing | don\'t | down | during | each | few | for | from | further | had | hadn\'t | has | hasn\'t | have | haven\'t | having | he | he\'d | he\'ll | he\'s | her | here | here\'s | hers | herself | him | himself | his | how | how\'s | i | i\'d | i\'ll | i\'m | i\'ve | if | in | into | is | isn\'t | it | it\'s | its | itself | let\'s | me | more | most | mustn\'t | my | myself | no | nor | not | of | off | on | once | only | or | other | ought | our | ours | ourselves | out | over | own | same | shan\'t | she | she\'d | she\'ll | she\'s | should | shouldn\'t | so | some | such | than | that | that\'s | the | their | theirs | them | themselves | then | there | there\'s | these | they | they\'d | they\'ll | they\'re | they\'ve | this | those | through | to | too | under | until | up | very | was | wasn\'t | we | we\'d | we\'ll | we\'re | we\'ve | were | weren\'t | what | what\'s | when | when\'s | where | where\'s | which | while | who | who\'s | whom | why | why\'s | with | won\'t | would | wouldn\'t | you | you\'d | you\'ll | you\'re | you\'ve | your | yours | yourself | yourselves )', ' ', e)
-    # Removes all the stopwords (words which are not characterizing for the meaning of the text)
-    for i in stop_words.get_stop_words('english'):
-        text = re.sub(' '+i+' ',' ',text)
-    print('Stop-words removed')
-    # Tokenize the clean text
     words = text.split(' ')
-    print('Tokenized')
-    print('There are ',len(words), ' words')
+    words.pop()
     # Stems the words
-    stemmed = [nltk.PorterStemmer().stem(t) for t in words]
-    print('Stemmed')
-    return stemmed
+    stemmed = []
+    errors = []
+    for word in words:
+        try:
+            stemmed.append(nltk.PorterStemmer().stem(word))
+        except IndexError:
+            errors.append(word)
+    if len(errors) != 0:
+        print('Warning: errors found during stemming. Follows list of not-working words.')
+        print(errors)		
+    return words
  
 
 # couples() returns all the combinations of two consecutives words  
