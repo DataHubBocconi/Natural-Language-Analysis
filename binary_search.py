94581fcc01b4c43743e6c8aabf29703b8b977a29
Guglielmo Gattiglio <g.gattiglio@gmail.com>
2017-12-03 22:57:26 +0100

    Update binary search module

--- binary_search.py
+++ binary_search.py
@@ -25,3 +25,20 @@ class binary_search():
             return self._binary_search(array, mid+1, high, value)
         else:
             return self._binary_search(array, low , mid, value)
+        
+    def binary_search_index(self, array,value):
+        return self._binary_search_index(array, 0, len(array)-1, value)
+    
+    def _binary_search_index(self, array, low, high, value):
+        if low == high:
+            if array[low] == value:
+                return low
+            else:
+                return False
+        mid = (low + high) // 2
+        if array[mid] == value:
+            return mid
+        elif array[mid] < value:
+            return self._binary_search_index(array, mid+1, high, value)
+        else:
+            return self._binary_search_index(array, low , mid, value)
