6c0f1baee746fa9afe28a9521d63bdd49f685d52
leodamico <leonardo.damico@studbocconi.it>
2017-12-19 17:43:28 +0100

    r code

--- LASSO/01_createdf.py
+++ LASSO/01_createdf.py
@@ -1,5 +1,8 @@
 import os, glob
+import pandas as pd
 from Cleaner import *
+from collections import defaultdict
+
 
 
 cwd = os.getcwd()
@@ -9,12 +12,30 @@ src = os.path.join(par, 'Sources')
 #zero_path = os.path.join(src, 'neutral')
 #one_path = os.path.join(src, 'racist')
 
-D = []
+oD = []
 for path, subdirs, files in os.walk(src):
     for name in files:
         v = 0
         if 'racist' in path:
             v=1
-        D.append([os.path.join(path, name),v])
-print(D)
+        oD.append([os.path.join(path, name),v,name])
 #stem each book and create dictionary
+D = []
+for d in oD:
+    D.append([clean_book(d[0]), d[1],d[2]])
+C = []
+for d in D:
+    C.append({})
+    for w in d[0]:
+        try:
+            C[-1][w] += 1
+        except KeyError:
+            C[-1][w] = 1
+    C[-1]['racist'] = d[1]
+    C[-1]['name'] = d[2]
+
+df = pd.DataFrame(C)
+print(df.head())
+df.fillna(0, inplace=True)
+print(df.head())
+df.to_csv(os.path.join(par, 'Data','rawdata.csv'))
